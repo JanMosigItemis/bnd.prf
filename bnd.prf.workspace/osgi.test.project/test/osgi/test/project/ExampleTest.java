5ef9410b7498a6dce0ee6d2edf5dd90e4da1c732
Jan Mosig <jan.mosig@itemis.de>
2018-07-15 21:17:35 +0200

    Fixed a couple of bugs and improved tests / gradle output a bit.

--- bnd.prf.workspace/osgi.test.project/test/osgi/test/project/ExampleTest.java
+++ bnd.prf.workspace/osgi.test.project/test/osgi/test/project/ExampleTest.java
@@ -2,13 +2,17 @@ package osgi.test.project;
 
 import static org.junit.Assert.*;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
 import org.junit.Test;
 
 public class ExampleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception{
+		int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
+	    System.err.println("Max AES Keylength: " + maxKeyLen + " --> " + (maxKeyLen > 128 ? "Unlimited " : "Limited ") + "Security.");
+        KeyGenerator.getInstance("SunTls12Prf");
 	}
-
 }
